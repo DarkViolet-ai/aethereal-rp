d889f603720a559af2347487dee212cf121bd5ca
= <=>
2023-12-09 13:56:17 -0500

    begin story working successfully

--- app/lib/ai/openaiGenerator.server.ts
+++ app/lib/ai/openaiGenerator.server.ts
@@ -1,14 +1,17 @@
 import OpenAI from "openai";
 const openai = new OpenAI();
 
-export const openaiGenerator = async (prompt: string, input: string) => {
+export const openaiGenerator = async (systemPrompt: string, input: string) => {
   const completion = await openai.chat.completions.create({
     messages: [
-      { role: "system", content: prompt },
+      { role: "system", content: systemPrompt },
       { role: "user", content: input },
     ],
     model: "gpt-4-1106-preview",
+    max_tokens: 2500,
+    response_format: {
+      type: "json_object",
+    },
   });
-
-  return completion.choices[0].message.content;
+  return completion.choices[0].message.content || "";
 };
--- app/routes/setup.tsx
+++ app/routes/setup.tsx
@@ -1,3 +1,5 @@
+import { DataFunctionArgs } from "@remix-run/node";
+import { typedjson, useTypedLoaderData } from "remix-typedjson";
 import Box from "~/components/buildingBlocks/box";
 import Button from "~/components/buildingBlocks/button";
 import Flex from "~/components/buildingBlocks/flex";
@@ -5,8 +7,30 @@ import Text from "~/components/buildingBlocks/text";
 import TextAreaVStack from "~/components/buildingBlocks/textAreaVStack";
 import VStack from "~/components/buildingBlocks/vStack";
 import { borderShadow } from "~/css/styles";
+import { beginStory } from "~/lib/ai/narrator.server";
+import { narratorInstructions } from "~/lib/ai/narratorInstructions";
+import { openaiGenerator } from "~/lib/ai/openaiGenerator.server";
+import { getStory, getUser } from "~/lib/db/db.server";
+import { dvError } from "~/lib/utils/dvError";
+
+export const loader = async ({ request }: DataFunctionArgs) => {
+  const user = await getUser("seed-user-id-1");
+  const initStory = await getStory({ id: "seed-story-id-1" });
+  if (!initStory || !user)
+    throw dvError.internalServerError("Could not find story or user");
+  initStory.version += 1;
+
+  const generator = openaiGenerator;
+  const { story, narrator, prompt } = await beginStory({
+    ...initStory,
+    narratorInstructions,
+    generator,
+  });
+  return typedjson({ story, narrator, prompt });
+};
 
 export default function Setup() {
+  const { story, narrator, prompt } = useTypedLoaderData<typeof loader>();
   return (
     <Flex className="w-full h-full justify-center pt-[50px] pb-[35px] items-center">
       <VStack className="w-full p-4" gap="gap-5">
@@ -14,7 +38,8 @@ export default function Setup() {
           className={`w-11/12 md:w-3/4 xl:w-2/3 xxl:w-1/2 h-[400px] ${borderShadow}`}
         >
           <Box className="w-full h-[400px] bg-dv-800 shadow-shadow3D p-2 overflow-y-auto">
-            <Text>TEXT</Text>
+            <Text>{story.content}</Text>
+            <Text>{prompt}</Text>
           </Box>
         </Box>
         <Box className="w-11/12 md:w-3/4  xl:w-2/3  xxl:w-1/2">
